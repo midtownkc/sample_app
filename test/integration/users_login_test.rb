c944517432c6c02d3401d2112ab582ee9a76b440
Maxwell Frederickson <maxwell@apple.com>
2017-11-15 11:08:45 -0600

    Implement advanced-login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -45,4 +45,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
     assert flash.empty?
   end
+
+  test 'login with remembering' do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+
+  test 'login without remembering' do 
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
+
 end
